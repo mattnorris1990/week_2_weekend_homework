7b9469e3b2cfe98faa9921ecee3f7adf8bad2f2a
mattnorris1990 <mattnorris1990@gmail.com>
2022-04-08 11:22:07 +0100

    added initial tests for guest, song

--- src/room.py
+++ src/room.py
@@ -1,2 +1,6 @@
 class Room:
-    
+    def __init__(self, input_name, input_capacity):
+        self.name = input_name
+        self.capacity = input_capacity
+        self.guest_list = []
+        self.song_list = []
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,6 +1,31 @@
 import unittest
 
-from room import *
+from src.room import *
+from src.guest import *
+from src.song import *
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
+        self.room_1 = Room("Room 1", 3)
+        self.room_2 = Room("Room 2", 5)
+        self.room_2 = Room("Room 3", 7)
+
+        self.song_1 = Song("Take on Me", "a-ha")
+        self.song_2 = Song("Wuthering Heights", "Kate Bush")
+        self.song_3 = Song("Bodies", "Drowning Pool")
+        self.song_4 = Song("Since U Been Gone", "Kelly Clarkson")
+
+        self.guest_1 = Guest("Stuart")
+        self.guest_2 = Guest("John")
+        self.guest_3 = Guest("Keith")
+        self.guest_4 = Guest("Kris")
+
+    def test_room_name(self):
+        self.assertEqual("Room 1", self.room_1.name)
+
+    def test_room_capacity(self):
+        self.assertEqual(3, self.room_1.capacity)
+
+    def test_add_to_guest_list(self):
+        self.add_guest_to_guest_list()
+        self.assertEqual("Stuart", self.guest_list)
