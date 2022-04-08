4ae3bcc31fb28a30578f078bb4d0a1a249b9ee21
mattnorris1990 <mattnorris1990@gmail.com>
2022-04-08 13:56:43 +0100

    added favourite song function to guest and test

--- src/guest.py
+++ src/guest.py
@@ -1,7 +1,14 @@
+from src.room import *
+
 class Guest:
 
     def __init__(self, input_name, input_wallet, input_favourite_song):
         self.name = input_name
         self.wallet = input_wallet
         self.favourite_song = input_favourite_song
+
+    def check_for_favourite_song(self, current_room):
+        for song in current_room.song_list:
+            if song == self.favourite_song:
+                return "THAT'S MY JAM OH YEAHHH"
 
--- tests/guest_test.py
+++ tests/guest_test.py
@@ -1,9 +1,20 @@
 import unittest
 
 from src.guest import *
+from src.room import *
+from src.song import *
 
 class TestGuest(unittest.TestCase):
     def setUp(self):
+        self.room_1 = Room("Room 1", 3, 10)
+        self.room_2 = Room("Room 2", 5, 15)
+        self.room_2 = Room("Room 3", 7, 20)
+
+        self.song_1 = Song("Take on Me", "a-ha")
+        self.song_2 = Song("Wuthering Heights", "Kate Bush")
+        self.song_3 = Song("Bodies", "Drowning Pool")
+        self.song_4 = Song("Since U Been Gone", "Kelly Clarkson")
+
         self.guest_1 = Guest("Stuart", 10, "Take on Me")
         self.guest_2 = Guest("John", 15, "Wuthering Heights")
         self.guest_3 = Guest("Keith", 20, "Bodies")
@@ -11,3 +22,11 @@ class TestGuest(unittest.TestCase):
 
     def test_guest_name(self):
         self.assertEqual("Stuart", self.guest_1.name)
+
+    def test_check_song_list_for_favourite_song(self):
+        self.room_1.add_song_to_song_list(self.song_1)
+        self.assertEqual("THAT'S MY JAM OH YEAHHH", self.guest_1.check_for_favourite_song(self.room_1))
+    
+    def test_check_song_list_for_favourite_song(self):
+        self.room_1.add_song_to_song_list(self.song_2)
+        self.assertEqual(None, self.guest_1.check_for_favourite_song(self.room_1))
